cdb9d7bf13537a611e6802876417bbdcc17fe613
Mark Basham <mark.basham@diamond.ac.uk>
2017-02-22 21:41:27 +0000

    update to accept credentials from the terminal

--- src/creds.py
+++ src/creds.py
@@ -2,11 +2,16 @@
 
 import json
 
+
+
 creds = {'consumer_key':'XXX',
          'consumer_secret':'XXX',
          'access_token':'XXX',
          'access_token_secret':'XXX'
          }
+
+for key in creds.keys():
+    creds[key] = input("Please enter %s : " % (key))
 
 with open('credentials.json', 'w') as fp:
     json.dump(creds, fp)
