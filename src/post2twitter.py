b54297acdf0a5e3d15b5a8fcc0af1d9b39ffd439
Mark Basham <mark.basham@diamond.ac.uk>
2017-02-23 15:41:40 +0000

    Reduce the verbosity of the logggig to the console

--- src/post2twitter.py
+++ src/post2twitter.py
@@ -21,34 +21,34 @@ parser.add_option("-c", "--credentials", dest="credential_dir",
 import logging
 import logging.handlers as handlers
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 syslog = handlers.SysLogHandler(address=(options.syslog, options.syslog_port))
 syslog.setFormatter(logging.Formatter('ProjectMTwitter:%(message)s'))
 syslog.setLevel(logging.DEBUG)
 logging.getLogger('').addHandler(syslog)
-logging.debug("Completed syslog setup")
+logging.info("Completed syslog setup")
 
 
 # Sort out twitter credentials
-logging.debug("Sort out twitter credentials")
+logging.info("Sort out twitter credentials")
 import os
 import twitter
 import json
 
 creds = None
 creds_path = os.path.join(options.credential_dir,'credentials.json')
-logging.debug("Path to creds is '%s'" % (creds_path))
+logging.info("Path to creds is '%s'" % (creds_path))
 
 with open(creds_path) as json_data:
     creds = json.load(json_data)
 
-logging.debug("credentials loaded")
+logging.info("credentials loaded")
 api = twitter.Api(consumer_key=creds['consumer_key'],
                   consumer_secret=creds['consumer_secret'],
                   access_token_key=creds['access_token'],
                   access_token_secret=creds['access_token_secret'])
 
-logging.debug("verifying credentials")
+logging.info("verifying credentials")
 logging.debug(api.VerifyCredentials())
 
 
@@ -56,7 +56,7 @@ logging.debug(api.VerifyCredentials())
 import numpy as np
 
 data_file_name = args[1]
-logging.debug("loading data file '%s'" % (data_file_name))
+logging.info("loading data file '%s'" % (data_file_name))
 a = np.loadtxt(data_file_name, unpack=True)
 
 
@@ -65,40 +65,40 @@ import pylab as pl
 import matplotlib.pyplot as plt
 
 fig1_file_name = os.path.join(options.temp_dir, 'pic1.png')
-logging.debug("Preparing '%s'" % (fig1_file_name))
+logging.info("Preparing '%s'" % (fig1_file_name))
 plt.close()
 plt.plot(a[0][100:7000],a[1][100:7000])
 plt.ylabel('Intensity')
-logging.debug("Saving '%s'" % (fig1_file_name))
+logging.info("Saving '%s'" % (fig1_file_name))
 pl.savefig(fig1_file_name, bbox_inches='tight')
 
 fig2_file_name = os.path.join(options.temp_dir, 'pic2.png')
-logging.debug("Preparing '%s'" % (fig2_file_name))
+logging.info("Preparing '%s'" % (fig2_file_name))
 plt.close()
 plt.plot(a[0][100:1000],a[1][100:1000])
 plt.ylabel('Intensity')
-logging.debug("Saving '%s'" % (fig2_file_name))
+logging.info("Saving '%s'" % (fig2_file_name))
 pl.savefig(fig2_file_name, bbox_inches='tight')
 
 fig3_file_name = os.path.join(options.temp_dir, 'pic3.png')
-logging.debug("Preparing '%s'" % (fig3_file_name))
+logging.info("Preparing '%s'" % (fig3_file_name))
 plt.close()
 plt.plot(a[0][1000:2000],a[1][1000:2000])
 plt.ylabel('Intensity')
-logging.debug("Saving '%s'" % (fig3_file_name))
+logging.info("Saving '%s'" % (fig3_file_name))
 pl.savefig(fig3_file_name, bbox_inches='tight')
 
 fig4_file_name = os.path.join(options.temp_dir, 'pic4.png')
-logging.debug("Preparing '%s'" % (fig4_file_name))
+logging.info("Preparing '%s'" % (fig4_file_name))
 plt.close()
 plt.plot(a[0][2000:3000],a[1][2000:3000])
 plt.ylabel('Intensity')
-logging.debug("Saving '%s'" % (fig4_file_name))
+logging.info("Saving '%s'" % (fig4_file_name))
 pl.savefig(fig4_file_name, bbox_inches='tight')
 
 
 # Post the update to twitter
-logging.debug("Posting update to twitter")
+logging.info("Posting update to twitter")
 status = api.PostUpdate('Data collection test 1/... for @basham_mark',
                         media=[fig1_file_name,
                                fig2_file_name,
